c5aa51651d05ecd5bcb0c023a51729a47050f8a5
Nerdk <univversehackers@gmail.com>
2025-08-01 07:54:48 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,14 +1,14 @@
 module.exports = {
-    command: ".menu",
-    run: async (sock, m) => {
-        const msg = `🌟 *KING-BOT MENU* 🌟
+  command: ".menu",
+  run: async (sock, m) => {
+    const msg = `🌟 *KING-BOT MENU* 🌟
 
 • .groupmenu
 • .music
 • .movie
 • .faketyping
-• .fakerecord
-`;
-        await sock.sendMessage(m.key.remoteJid, { text: msg }, { quoted: m });
-    }
+• .fakerecord`;
+
+    await sock.sendMessage(m.key.remoteJid, { text: msg }, { quoted: m });
+  }
 };
